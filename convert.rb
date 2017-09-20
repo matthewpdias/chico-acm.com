dc398f3188e21980cfedf2d08f2c91fbd8c8519a
matthewpdias <matthewpdias@gmail.com>
2017-09-20 08:15:32 -0700

    chaged directory structure and removed extranous files

--- convert.rb
+++
@@ -1,7 +0,0 @@
-puts "Enter the name of the file you want to convert:"
-
-input = gets.chomp
-
-output = input.sub('.md', '.html')
-
-system("showdown", "makehtml", "-i", "#{input}", "-o", "#{output}")
